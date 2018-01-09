29d020b448478af88f3f0f71d7fd1853e4c6645e
mla <miluna1994@gmail.com>
2018-01-09 20:17:46 +0100

    Test clase estatica Math

---
+++ src/com/cice/mla/Math/Main.java
@@ -0,0 +1,12 @@
+package com.cice.mla.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 123456;
+        double b = Math.pow(a, 2);
+
+        System.out.println(b);
+
+    }
+
+}
