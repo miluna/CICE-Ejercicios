a890877015b6ba5e958b97e4cda774a27a7928c1
Miguel Angel Luna <miluna1994@gmail.com>
2018-07-10 21:53:44 +0200

    Zuul server

---
+++ spring-zuul-server/src/main/java/com/cice/springzuulserver/SpringZuulServerApplication.java
@@ -0,0 +1,16 @@
+package com.cice.springzuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class SpringZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulServerApplication.class, args);
+	}
+}
